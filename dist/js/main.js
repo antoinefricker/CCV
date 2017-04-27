d191dbcd2f9fdf634c13141915c46b5e921ae3b2
Antoine FRICKER <antoine.fricker@gmail.com>
2017-04-27 09:01:08 +0200

    Refactoring styles

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
+	var ppage, p;
 	
 	KPF.utils.log('---------------------------------- DOCUMENT READY');
 	
 	
+	// --------------- home page
 	
+	// buttons actions
 	$('#home')
 		.on('click', 'button.action-info', function(){
 			DGN.app.pageSet('info');
@@ -12,16 +15,31 @@ $(document).ready(function(){
 			DGN.app.pageSet('play');
 		});
 	
+	
+	// --------------- help page
+	
+	// buttons actions
 	$('#help')
 		.on('click', 'button.action-close', function(){
 			DGN.app.pageSet('play');
 		});
 	
-	$('#info')
-		.on('click', 'button.action-home', function(){
+	
+	// --------------- info page
+	ppage = $('#info');
+	
+	// buttons actions
+	ppage.on('click', 'button.action-home', function(){
 			DGN.app.pageSet('home');
 		});
 	
+	p = ppage.find('pagination');
+	ppage.children('.inner-page').each(function(index, el){
+		$('<div>' + index + '</div>')
+	});
+	
+	
+	// --------------- play page
 	$('#play')
 		.on('click', 'button.action-help', function(){
 			DGN.app.pageSet('help');
